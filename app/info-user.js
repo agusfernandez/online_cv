df9d5aab01ee76791f3af58213c1586bedf763f3
agusfernandez7 <agustina.fernandez@improntus.com>
2023-04-18 14:04:09 -0300

    cambios nuevos

--- app/info-user.js
+++ app/info-user.js
@@ -1,6 +1,7 @@
 /* Show Personal Info */
 
 const infoToggle = document.querySelectorAll('.icon-info');
-infoToggle.classList.add('color');
 const iconsToggle = document.querySelectorAll('.bi');
+const infoIcon = document.getElementsByClassName('.info-icon');
+infoIcon.classList.contains('show');
 
